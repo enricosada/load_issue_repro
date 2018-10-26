c0848ec721ece4f76f75091550409517a3683360
Enrico Sada <enrico.sada@jet.com>
2018-10-26 17:38:21 +0200

    add code

---
+++ fails.fsx
@@ -0,0 +1,10 @@
+
+// #load ".paket/load/FSharp.CLI.fsx"
+// #load ".paket/load/FSharp.Data.fsx"
+
+#load ".paket/load/Expecto.Hopac.fsx"
+#load ".paket/load/Expecto.FsCheck.fsx"
+#load ".paket/load/Expecto.fsx"
+
+// #load ".paket/load/net462/Main3/Marvel.DrOrpheus.fsx"
+// #load ".paket/load/net462/Main3/Marvel.Consul.fsx"
